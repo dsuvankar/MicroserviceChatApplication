2cc6bf24ab22dad73125cca08eccc61adf3b06ff
Suvankar Das <iamsuvankar25@gmail.com>
2025-11-11 00:13:55 +0530

    done

--- backend/userService/dist/model/User.d.ts
+++ backend/userService/dist/model/User.d.ts
@@ -1,7 +1,11 @@
-import { Document } from "mongoose";
+import mongoose, { Document } from "mongoose";
 export interface IUser extends Document {
     name: string;
     email: string;
 }
-export declare const User: any;
+export declare const User: mongoose.Model<IUser, {}, {}, {}, mongoose.Document<unknown, {}, IUser, {}, {}> & IUser & Required<{
+    _id: unknown;
+}> & {
+    __v: number;
+}, any>;
 //# sourceMappingURL=User.d.ts.map
